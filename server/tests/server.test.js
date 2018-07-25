40bba681aff5adcf88bb993997aa7e7587727815
hdua1@asu.edu <hdua1@asu.edu>
2018-07-25 11:39:59 -0700

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -32,8 +32,8 @@ describe('POST /todos',()=>{
 				done();
 			}).catch((e)=>{
 				done(e);
-			})
-		})
+			});
+		});
 	});
 	it('should not create todo with invalid body data',(done)=>{
 		request(app)
@@ -284,6 +284,24 @@ describe('POST /users/login',()=>{
 				return done(err);
 			}
 			User.findById(users[1]._id).then((user)=>{
+				expect(user.tokens.length).toBe(0);
+				done();
+			}).catch((e)=>done(e));
+		});
+	});
+});
+
+describe('DELETE /users/me/token',()=>{
+	it('should remove auth token on logout',(done)=>{
+		request(app)
+		.delete('/users/me/token')
+		.set('x-auth',users[0].tokens[0].token)
+		.expect(200)
+		.end((err,res)=>{
+			if(err){
+				return done(err);
+			}
+			User.findById(users[0]._id).then((user)=>{
 				expect(user.tokens.length).toBe(0);
 				done();
 			}).catch((e)=>done(e));
